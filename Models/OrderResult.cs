ebcdd0286d1c1d7ff87f16ce0b8eba45c5d1a25c
RichiJerryTsai <jerrytsai@richitech.com.tw>
2026-06-02 09:02:20 +0800

    Modify alignment

--- Models/OrderResult.cs
+++ Models/OrderResult.cs
@@ -5,6 +5,7 @@ namespace PracticeForCSharp.Models;
 public class OrderResult
 {
     [JsonPropertyName("訂單編號")]
+
     public int OrderId { get; set; }
 
     [JsonPropertyName("訂購日期")]
@@ -12,6 +13,7 @@ public class OrderResult
     public string OrderDate { get; set; }="";
 
     [JsonPropertyName("訂購金額")]
+    
     public decimal TotalAmount{ get; set; }
 
     [JsonPropertyName("訂購商品清單")]
